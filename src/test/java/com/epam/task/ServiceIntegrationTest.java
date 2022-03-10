f384417c292a318db1345567c40bf80e7362616d
Andruha125 <a.sokorenko@gmail.com>
2022-03-10 16:50:11 +0200

    added service integration tests

---
+++ src/test/java/com/epam/task/ServiceIntegrationTest.java
@@ -0,0 +1,48 @@
+package com.epam.task;
+
+import com.epam.task.model.UserModel;
+import com.epam.task.repository.UserRepository;
+import com.epam.task.service.UserService;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.TestPropertySource;
+
+@TestPropertySource("/applicationTest.yaml")
+@SpringBootTest
+public class ServiceIntegrationTest {
+    @Autowired
+    private UserRepository userRepository;
+
+    @Autowired
+    private UserService userService;
+
+    @Test
+    public void test1() {
+
+        userService.moveToken(1, 5);
+        userService.moveToken(1, 3);
+
+        UserModel user1 = userService.getCurrentPosition(1);
+
+        Assertions.assertEquals(9, user1.getCurrentPlace());
+
+
+    }
+    @Test
+    public void test2() {
+
+        userRepository.save(new UserModel(1, 94));
+
+        userService.moveToken(1, 5);
+        userService.moveToken(1, 3);
+
+        UserModel user1 = userService.getCurrentPosition(1);
+
+        Assertions.assertEquals(99, user1.getCurrentPlace());
+
+
+    }
+
+}

91a2d1853571f946f3098edf2b5e009326f60f0c
Andruha125 <a.sokorenko@gmail.com>
2022-03-17 13:42:01 +0200

    service impl

--- src/main/java/com/epam/task/service/UserService.java
+++ src/main/java/com/epam/task/service/UserService.java
@@ -2,9 +2,14 @@ package com.epam.task.service;
 
 import com.epam.task.dto.DieDto;
 import com.epam.task.dto.UserDto;
+import com.epam.task.exception.CanNotAddUserWhenGameIsStartedException;
+import com.epam.task.exception.DieAlreadyRolledException;
+import com.epam.task.exception.DieMustBeRolledBeforeMakingMoveException;
+import com.epam.task.model.SnakesAndLaddersModel;
 import com.epam.task.model.UserModel;
 import com.epam.task.repository.SnakesAndLaddersRepository;
 import com.epam.task.repository.UserRepository;
+import com.epam.task.util.Utils;
 import org.springframework.stereotype.Service;
 
 import java.util.*;
@@ -21,26 +26,64 @@ public class UserService {
     }
 
     public UserDto makeMove(){
-        return null;
+        UserModel user = userRepository.findByCurrentTrue();
+
+        int step = user.getStep();
+            if (step == 0) throw new DieMustBeRolledBeforeMakingMoveException();
+
+        int nextPlace = user.getCurrentPlace() + step > 100 ? user.getCurrentPlace() : user.getCurrentPlace() + step;
+
+        Optional<SnakesAndLaddersModel> jump = snakesAndLaddersRepository.findByFromPositionEquals(nextPlace);
+
+        if (jump.isPresent()) nextPlace = jump.get().getToPosition();
+
+        userRepository.save(new UserModel(user.getId(), nextPlace, 0, false));
+
+        Optional<UserModel> nextUser = userRepository.findById(user.getId() + 1);
+
+        if (nextUser.isPresent()){
+            nextUser.get().setCurrent(true);
+            userRepository.save(nextUser.get());
+        } else {
+            UserModel userModel = userRepository.getUserModelById(1);
+            userModel.setCurrent(true);
+            userRepository.save(userModel);
+        }
+
+        return new UserDto(user.getId(), nextPlace);
 
     }
 
     public DieDto rollDie(){
 
 
-        return null;
+        Random random = new Random();
+        int num = random.nextInt(6) + 1;
+
+        int rolled = userRepository.rollDie(num);
+        if (rolled == 0) throw new DieAlreadyRolledException();
+        return DieDto.builder().rolledNum(num).build();
     }
 
     public UserDto addUser(){
-        return null;
+        List<UserModel> users = userRepository.findAll();
+        users.forEach( user -> {if (user.getCurrentPlace() != 1)
+            throw new CanNotAddUserWhenGameIsStartedException();  });
+
+        UserModel userModel = new UserModel(users.size() + 1, 1, 0, false);
+        userRepository.save(userModel);
+        return Utils.UserModelToUserDtoTransformer(userModel);
     }
 
     public List<UserModel> findUsers(){
-        return null;
+        return userRepository.findAll();
     }
 
 
     public UserModel restartGame(){
-        return null;
+        userRepository.deleteAll();
+        UserModel userModel = new UserModel(1,1,0, true);
+        userModel = userRepository.save(userModel);
+        return userModel;
     }
 }
